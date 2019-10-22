d1e1ccea020f001021b470cfac0e89f723d75ea4
Howard <nzhoward@gmail.com>
2019-10-21 22:08:22 -0400

    Update day1.py

--- solutions/day1.py
+++ solutions/day1.py
@@ -1,26 +1,27 @@
-inputList = [10, 15, 3, 7]
-k = 17
+nums_1 = [10, 15, 3, 7]
+k1 = 17
 k2 = 20
 
-def naive(inputList, inputK):
-    for i in range(len(inputList)):
-        for j in range(i + 1, len(inputList)):
-            if inputList[i] + inputList[j] == inputK:
-                return True;
-    return False;
+
+def naive(nums, k):
+    for i in range(len(nums)):
+        for j in range(i + 1, len(nums)):
+            if nums[i] + nums[j] == k:
+                return True
+    return False
 
 
-assert naive(inputList, k) == True
-assert naive(inputList, k2) == False
+assert naive(nums_1, k1)
+assert not naive(nums_1, k2)
 
 
-def onePass(inputList, inputK):
+def one_pass(nums, k):
     val = {}
-    for i in range(inputK + 1):
+    for i in range(k + 1):
         val[i] = False
 
-    for i in inputList:
-        if val[inputK - i] == True:
+    for i in nums:
+        if val[k - i]:
             return True
         elif val[i] == 0:
             val[i] = True
@@ -28,5 +29,5 @@ def onePass(inputList, inputK):
     return False
 
 
-assert onePass(inputList, k) == True
-assert onePass(inputList, k2) == False
+assert one_pass(nums_1, k1)
+assert not one_pass(nums_1, k2)
