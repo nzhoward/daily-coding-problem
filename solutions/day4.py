03a8b981ffc3e40f9543ec004a5e59c19d7a6e56
Howard <nzhoward@gmail.com>
2019-10-21 22:09:45 -0400

    Update day4.py

--- solutions/day4.py
+++ solutions/day4.py
@@ -1,35 +1,36 @@
-inputList = [3, 4, -1, 1]
-inputList2 = [1, 2, 0]
-inputList3 = [2, 1]
+nums_1 = [3, 4, -1, 1]
+nums_2 = [1, 2, 0]
+nums_3 = [2, 1]
 
 
-def missingPositiveInt(inputList):
-    n = len(inputList)
-    if 1 not in inputList:
+def missing_positive_int(nums):
+    n = len(nums)
+    if 1 not in nums:
         return 1
     for i in range(n):
-        if inputList[i] < 0 or inputList[i] > n or inputList[i] == 0:
-            inputList[i] = 1
+        if nums[i] < 0 or nums[i] > n or nums[i] == 0:
+            nums[i] = 1
 
-    print(inputList)
+    print(nums)
 
     for i in range(n):
-        a = abs(inputList[i])
+        a = abs(nums[i])
         if a == n:
-            inputList[0] = - abs(inputList[0])
+            nums[0] = - abs(nums[0])
         else:
-            inputList[a] = - abs(inputList[a])
+            nums[a] = - abs(nums[a])
 
-    print(inputList)
+    print(nums)
 
     for i in range(1, n):
-        if inputList[i] > 0:
+        if nums[i] > 0:
             return i
-    if inputList[0] > 0:
+    if nums[0] > 0:
         return n
 
     return n + 1
-        
-assert missingPositiveInt(inputList) == 2
-assert missingPositiveInt(inputList2) == 3
-assert missingPositiveInt(inputList3) == 3
+
+
+assert missing_positive_int(nums_1) == 2
+assert missing_positive_int(nums_2) == 3
+assert missing_positive_int(nums_3) == 3
